63fe6b2a14c71f4f4c2115bdab5fd1a765650d0d
Andrii <110056592+andriimonastyrskyi@users.noreply.github.com>
2023-07-08 11:23:28 +0300

    fix

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use(express.static("public"));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-app.use("/api/auth", authRouter);
+app.use("/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
